f287e5afdb0499cfcc7125ef0ba067d20f9ce4e0
endomorphosis <starworks5@gmail.com>
2024-08-29 20:03:54 -0700

    chore: Add libp2p_kit_js files, update package.json, and export libp2pKitJs as default

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export * from './libp2p_kit_js/websocket_kit.js';
 export * from './libp2p_kit_js/s3_kit.js';
 export * from './libp2p_kit_js/aria2_kit.js';
 export * from './libp2p_kit_js/main.js';
+export { libp2pKitJs as default } from './libp2p_kit_js/libp2p_kit.js';
---
+++ libp2p_kit_js/index.js
@@ -0,0 +1,3 @@
+export * from './libp2p_kit_js/libp2p_kit.js';
+export { libp2pKitJs as default } from './libp2p_kit_js/libp2p_kit.js';
+export { libp2pKitJs } from './libp2p_kit_js/libp2p_kit.js';
