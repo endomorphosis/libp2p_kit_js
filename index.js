1bdab2d44b9b9163905e33e38f6a2d3661c6aade
endomorphosis <starworks5@gmail.com>
2024-09-01 21:38:18 -0700

    update

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ export * from './libp2p_kit_js/libp2p.js';
 export * from './libp2p_kit_js/websocket_kit.js';
 export { libp2pKitJs } from './libp2p_kit_js/libp2p_kit.js'
 export { libp2pKitJs as default } from './libp2p_kit_js/libp2p_kit.js';
+
+export * from './test/test.js'
+export { test_libp2p_kit_js } from './test/test.js'
+const test = new test_libp2p_kit_js();
+export { test }
