1ed1817736131339ef6da52c8d97d1ffdbd1206d
endomorphosis <starworks5@gmail.com>
2024-10-20 22:42:32 -0700

    Refactor libp2p.js and libp2p_kit.js files

--- index.js
+++ index.js
@@ -8,5 +8,3 @@ export { libp2pKitJs as default } from './libp2p_kit_js/libp2p_kit.js';
 export * from './test/test.js'
 import { test_libp2p_kit_js } from './test/test.js'
 export { test_libp2p_kit_js } from './test/test.js'
-const test = new test_libp2p_kit_js();
-export { test }
