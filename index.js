58c1d69dbbf295afdc00c04fed181a41a5b73034
endomorphosis <starworks5@gmail.com>
2024-10-26 01:02:13 -0700

    Refactor libp2p.js and libp2p_kit.js files, and update import paths

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ export * from './libp2p_kit_js/aria2_kit.js';
 export * from './libp2p_kit_js/s3_kit.js';
 export * from './libp2p_kit_js/libp2p.js';
 export * from './libp2p_kit_js/websocket_kit.js';
-export { libp2pKitJs } from './libp2p_kit_js/libp2p_kit.js'
-export { libp2pKitJs as default } from './libp2p_kit_js/libp2p_kit.js';
+export * from './libp2p_kit_js/libp2p_kit.js';
+import { libp2pKitJs } from './libp2p_kit_js/index.js'
+export { libp2pKitJs } from './libp2p_kit_js/index.js'
+export { libp2pKitJs as default } from './libp2p_kit_js/index.js';
 export * from './libp2p_kit_js/index.js';
 export * from './test/test.js'
 import { test_libp2p_kit_js } from './test/test.js'
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import { libp2pKitJs } from "../libp2p_kit_js/index.js"
+import { libp2pKitJs } from "../index.js"
 // import { requireConfig } from "../config/config.js";
 import path from "path";
 import fs from "fs";
